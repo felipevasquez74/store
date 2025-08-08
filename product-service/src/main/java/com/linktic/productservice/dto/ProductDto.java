5e96f03cd6a2e50383c1462a29adec0b456709ae
andres vasquez <andres.vasquez@aldeamo.com>
2025-08-08 17:21:27 -0500

    change record product to class

--- product-service/src/main/java/com/linktic/productservice/dto/ProductDto.java
+++ product-service/src/main/java/com/linktic/productservice/dto/ProductDto.java
@@ -2,7 +2,13 @@ package com.linktic.productservice.dto;
 
 import io.katharsis.resource.annotations.JsonApiId;
 import io.katharsis.resource.annotations.JsonApiResource;
+import lombok.Data;
 
 @JsonApiResource(type = "products")
-public record ProductDto(@JsonApiId String id, String name, String price) {
+@Data
+public class ProductDto {
+	@JsonApiId
+	private String id;
+	private String name;
+	private String price;
 }
