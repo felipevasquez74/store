3599a704c000b51d5eb29252d9cb87edec197a47
andres vasquez <andres.vasquez@aldeamo.com>
2025-08-09 10:40:04 -0500

    add dockerfile and docker compose

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM maven:3.9.6-eclipse-temurin-21 AS build
+WORKDIR /app
+COPY . .
+ARG SERVICE
+RUN mvn clean package -pl ${SERVICE} -am -DskipTests
+
+FROM eclipse-temurin:21-jre
+WORKDIR /app
+ARG SERVICE
+COPY --from=build /app/${SERVICE}/target/*.jar app.jar
+EXPOSE 8080
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- product-service/src/main/java/com/linktic/productservice/config/OpenApiConfig.java
+++ product-service/src/main/java/com/linktic/productservice/config/OpenApiConfig.java
@@ -3,17 +3,22 @@ package com.linktic.productservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
     OpenAPI productServiceOpenAPI() {
+        final String securitySchemeName = "X-API-KEY";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Product Service API")
@@ -27,6 +32,13 @@ public class OpenApiConfig {
                                 .url("http://springdoc.org")))
                 .externalDocs(new ExternalDocumentation()
                         .description("Technical documentation")
-                        .url("https://linktic.com/docs"));
+                        .url("https://linktic.com/docs"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name(securitySchemeName)));
     }
 }
