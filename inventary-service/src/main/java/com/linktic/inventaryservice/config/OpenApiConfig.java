4d180d54d9438530c2b66f83e9a1e95d90b5e17d
andres vasquez <andres.vasquez@aldeamo.com>
2025-08-09 17:37:51 -0500

    swagger config bean

---
+++ inventary-service/src/main/java/com/linktic/inventaryservice/config/OpenApiConfig.java
@@ -0,0 +1,44 @@
+package com.linktic.inventaryservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    OpenAPI productServiceOpenAPI() {
+        final String securitySchemeName = "X-API-KEY";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Inventary Service API")
+                        .description("API for Inventary management following JSON:API specification")
+                        .version("v1.0")
+                        .contact(new Contact()
+                                .name("Linktic Team")
+                                .email("support@linktic.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Technical documentation")
+                        .url("https://linktic.com/docs"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name(securitySchemeName)));
+    }
+}
